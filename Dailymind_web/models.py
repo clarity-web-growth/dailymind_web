9f8ca0810cf6fb60e7147f46dc9172164e7bc91e
clarity-web-growth <louisandoh63@gmail.com>
2026-01-20 13:52:00 +0000

    Push to GitHub

--- Dailymind_web/models.py
+++ Dailymind_web/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    license_key = db.Column(db.String(32), nullable=False)
     subscription = db.Column(db.String(20), default="free")
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    license_key = db.Column(db.String(64))
+    subscription_expires = db.Column(db.DateTime, nullable=True)
 
