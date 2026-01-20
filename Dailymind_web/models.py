c9d3f2cc76c11d6c0bed55578b39622035947fad
clarity-web-growth <louisandoh63@gmail.com>
2026-01-20 14:24:26 +0000

    Push to GitHub

--- Dailymind_web/models.py
+++ Dailymind_web/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
 
 db = SQLAlchemy()
 
@@ -7,6 +7,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     subscription = db.Column(db.String(20), default="free")
-    license_key = db.Column(db.String(64))
-    subscription_expires = db.Column(db.DateTime, nullable=True)
+    license_key = db.Column(db.String(32))
+    subscription_expires = db.Column(db.DateTime)
+
+    def activate_premium(self, days=30):
+        self.subscription = "premium"
+        self.subscription_expires = datetime.utcnow() + timedelta(days=days)
+
 
