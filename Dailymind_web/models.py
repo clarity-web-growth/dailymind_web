ca95f7361ea9a366f61ecfae9a4eab58dc4ff662
clarity-web-growth <louisandoh63@gmail.com>
2026-01-20 09:15:27 +0000

    Push to GitHub

--- Dailymind_web/models.py
+++ Dailymind_web/models.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.String(64), unique=True, nullable=False)
+    license_key = db.Column(db.String(32), nullable=True)
+    subscription = db.Column(db.String(20), default="free")
+
+    def __repr__(self):
+        return f"<User {self.device_id}>"
